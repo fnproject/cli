b4f734b604ff8c73cb521770047f4df7fe87fe20
Dario Domizioli <dario.domizioli@oracle.com>
2017-09-14 15:15:53 +0100

    java: use our custom build image

--- langs/java.go
+++ langs/java.go
@@ -17,7 +17,7 @@ type JavaLangHelper struct {
 }
 
 // BuildFromImage returns the Docker image used to compile the Maven function project
-func (lh *JavaLangHelper) BuildFromImage() string { return "maven:3.5-jdk-8-alpine" }
+func (lh *JavaLangHelper) BuildFromImage() string { return "fnproject/fn-java-fdk:dev" }
 
 // RunFromImage returns the Docker image used to run the Java function.
 func (lh *JavaLangHelper) RunFromImage() string {
