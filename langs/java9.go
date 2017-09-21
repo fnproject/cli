e725d5065ac432afe931007b313a1bdc4f68fc1e
Lloyd Ollerhead <lloyd.ollerhead@oracle.com>
2017-09-21 16:02:29 +0100

    Added skeleton for java9 langHelper

--- langs/java9.go
+++ langs/java9.go
@@ -1,10 +1,42 @@
 package langs
 
+import "errors"
+
 type Java9LangHelper struct {
 	BaseHelper
 }
 
+func (lh *Java9LangHelper) BuildFromImage() string {
+	//FIXME: assumptions made here about dockerhub name
+	//should this be a funcy: repo?
+	return "fnproject/fn-java-fdk-build-1.9:latest"
+}
+
+func (lh *Java9LangHelper) RunFromImage() string {
+	//FIXME: assumptions made here about dockerhub name
+	return "fnproject/fn-java-fdk-1.9:latest"
+}
+
 // HasBoilerplate returns false as a stub until java9 boilerplate has been implemented
 func (lh *Java9LangHelper) HasBoilerplate() bool {
-	return false
+	return false //FIXME @ollerhll
+}
+
+func (lh *Java9LangHelper) GenerateBoilerPlate() error {
+	return errors.New("No boilerplate generation has been implemented for java9 yet.") //FIXME @ollerhll
+}
+
+// Cmd currently returns nil as boilerplate has not yet been implemented
+func (lh *Java9LangHelper) Cmd() string {
+	return nil //FIXME @ollerhll
+}
+
+// DockerfileBuildCmds currently returns nil as boilerplate has not yet been implemented
+func (lh *Java9LangHelper) DockerfileBuildCmds() []string {
+	return nil //FIXME @ollerhll
+}
+
+// HasPreBuild returns false as a stub until prebuild has been implemented
+func (lh *Java9LangHelper) HasPreBuild() bool {
+	return false //FIXME @ollerhll
 }
