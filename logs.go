9f27d57620219f415b0849e4944f878072ac2c90
Travis Reeder <treeder@gmail.com>
2017-11-06 10:27:11 +0100

    Adds support for "last" to get last logs (#83)
    * Adds support for "last" to get last logs
    
    * Upgraded deps and fix issue.

--- logs.go
+++ logs.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	client "github.com/fnproject/cli/client"
 	fnclient "github.com/fnproject/fn_go/client"
+	ccall "github.com/fnproject/fn_go/client/call"
 	apicall "github.com/fnproject/fn_go/client/operations"
 	"github.com/urfave/cli"
 )
@@ -24,7 +26,7 @@ func logs() cli.Command {
 			{
 				Name:      "get",
 				Aliases:   []string{"g"},
-				Usage:     "get function call log info per app",
+				Usage:     "get logs for a call. Must provide call_id or last to retrieve the most recent calls logs.",
 				ArgsUsage: "<app> <call-id>",
 				Action:    c.get,
 			},
@@ -34,6 +36,27 @@ func logs() cli.Command {
 
 func (log *logsCmd) get(ctx *cli.Context) error {
 	app, callID := ctx.Args().Get(0), ctx.Args().Get(1)
+	if callID == "last" {
+		params := ccall.GetAppsAppCallsParams{
+			App:     app,
+			Context: context.Background(),
+		}
+		resp, err := log.client.Call.GetAppsAppCalls(&params)
+		if err != nil {
+			switch e := err.(type) {
+			case *ccall.GetAppsAppCallsNotFound:
+				return errors.New(e.Payload.Error.Message)
+			default:
+				return err
+			}
+		}
+		calls := resp.Payload.Calls
+		if len(calls) > 0 {
+			callID = calls[0].ID
+		} else {
+			return errors.New("No previous calls found.")
+		}
+	}
 	params := apicall.GetAppsAppCallsCallLogParams{
 		Call:    callID,
 		App:     app,
