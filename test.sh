163138f8ae066c2e0361497b799ca05588363786
Denis Makogon <lildee1991@gmail.com>
2018-02-24 01:30:59 +0200

    Go: use vendor or dep tool during builds (#178)
    * Go: use vendor or dep tool during builds
    
    Closes: #141
    
    * Address review comments

--- test.sh
+++ test.sh
@@ -82,4 +82,19 @@ $fn call myapp1 /$funcname
 $fn routes create myapp1 /another --image $FN_REGISTRY/$funcname:0.0.2
 $fn call myapp1 /another
 
-
+# Test go func
+cd $WORK_DIR
+funcname="gofunc"
+mkdir $funcname
+cd $funcname
+$fn init --runtime go
+rm func.go
+curl -L https://raw.githubusercontent.com/fnproject/fdk-go/master/examples/hello/func.go -o func.go
+curl -L https://raw.githubusercontent.com/fnproject/fdk-go/master/examples/hello/Gopkg.toml -o Gopkg.toml
+curl -L https://raw.githubusercontent.com/fnproject/fdk-go/master/examples/hello/Gopkg.lock -o Gopkg.lock
+# checking how CLI works with dep tool
+$fn -v build
+# checking how CLI works with vendor
+$fn -v build
+$fn run
+$fn test
