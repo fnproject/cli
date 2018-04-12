917420211284b0126eef5d366b9cb37bf36ed181
Travis Reeder <treeder@gmail.com>
2018-04-12 08:56:11 -0700

    Can't seem to find a field that's comparable on local vs docker hub... :(

--- start.go
+++ start.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"os/signal"
 	"syscall"
+	"time"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
 	"github.com/urfave/cli"
 )
 
@@ -38,8 +45,9 @@ func start(c *cli.Context) error {
 	if err != nil {
 		log.Fatalln("Getwd failed:", err)
 	}
+	cname := "fnserver"
 	args := []string{"run", "--rm", "-i",
-		"--name", "fnserver",
+		"--name", cname,
 		"-v", fmt.Sprintf("%s/data:/app/data", wd),
 		"-v", "/var/run/docker.sock:/var/run/docker.sock",
 		"--privileged",
@@ -68,10 +76,13 @@ func start(c *cli.Context) error {
 	go func() {
 		done <- cmd.Wait()
 	}()
+
+	// check for newer version:
+	go checkForNewerVersion(cname)
+
 	// catch ctrl-c and kill
 	sigC := make(chan os.Signal, 2)
 	signal.Notify(sigC, os.Interrupt, syscall.SIGTERM)
-
 	for {
 		select {
 		case <-sigC:
@@ -89,4 +100,70 @@ func start(c *cli.Context) error {
 		return err
 	}
 	return nil
+}
+
+type TagResponse struct {
+	Results []Tag `json:"results"`
+}
+type Tag struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	FullSize int64  `json:"full_size"`
+}
+
+func checkForNewerVersion(cname string) {
+	ctx := context.Background()
+	time.Sleep(3 * time.Second)
+
+	// Grab latest tags
+	resp, err := http.Get("https://registry.hub.docker.com/v2/repositories/fnproject/fnserver/tags")
+	if err != nil {
+		log.Println("Error getting tags: %s", err)
+	}
+	defer resp.Body.Close()
+	tagR := &TagResponse{}
+	err = json.NewDecoder(resp.Body).Decode(tagR)
+	fmt.Printf("tagR: %+v\n", tagR)
+
+	dc, err := client.NewEnvClient()
+	if err != nil {
+		log.Println("Error instantiating Docker client: %s", err)
+		return
+	}
+
+	cJSON, err := dc.ContainerInspect(ctx, cname)
+	if err != nil {
+		log.Println("Error getting fnserver container info: %s", err)
+		return
+	}
+	fmt.Printf("cJSON: %+v\n", cJSON)
+
+	args := filters.NewArgs()
+	args.Add("reference", "fnproject/fnserver")
+	imgList, err := dc.ImageList(ctx, types.ImageListOptions{
+		Filters: args,
+	})
+	if err != nil {
+		log.Printf("Error getting image list: %s", err)
+		return
+	}
+	for _, img := range imgList {
+		fmt.Printf("image: %+v\n", img)
+		if len(img.RepoTags) > 0 {
+			if img.RepoTags[0] == "fnproject/fnserver:latest" {
+
+			}
+		}
+	}
+
+	// newImageInfo, _, err := dc.ImageInspectWithRaw(ctx, functionsDockerImage)
+	// if err != nil {
+	// 	log.Println("Error inspecting image: %s", err)
+	// 	return
+	// }
+
+	// if newImageInfo.ID != cJSON.Image {
+	// 	log.Println("Found new fnserver image, try updating with `fn update` to get latest version.")
+	// 	return
+	// }
 }
