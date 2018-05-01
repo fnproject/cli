516f866a29b1164638a1f9f93dd04e00a15d9ccf
JadeRedworth <jaderedworth@hotmail.co.uk>
2018-05-01 15:04:30 -0700

    Added 'update' context command to edit current context files. This reads in the context file and identifies if the <key> provided by the user is valid within the current context file. If so updates the <value> if not users are prompted with relevant messages.
    Also some code refactoring.
    
    validate api url
    
    Added validation for api-url when creating and updating and added
    tests for this validation.
    
    added the ability to add new key/values into the context file
    
    Uncomplicated update command, added a contextMap type and Set method
    for better readability and UX.
    
    Created a utils folder for decoding/writing yaml files, it did not need
    to be within config/config.go.
    
    A more specific api-url validation that follows the same pattern as the
    client/api checks.
    
    moved GetHomeDir() to utils file
    
    refactored decode/write yaml files
    
    update context.md

--- context_test.go
+++ context_test.go
@@ -11,11 +11,11 @@ func TestValidateContextName(t *testing.T) {
 	}{
 		{name: "local", expectedErr: ""},
 		{name: "Local", expectedErr: ""},
-		{name: "../local", expectedErr: "please enter a context name with only Alphanumeric, _, or -"},
 		{name: "local-context", expectedErr: ""},
 		{name: "local_context", expectedErr: ""},
 		{name: "local1", expectedErr: ""},
 		{name: "local-context-1", expectedErr: ""},
+		{name: "../local", expectedErr: "please enter a context name with only Alphanumeric, _, or -"},
 		{name: "local?context", expectedErr: "please enter a context name with only Alphanumeric, _, or -"},
 		{name: "context>?", expectedErr: "please enter a context name with only Alphanumeric, _, or -"},
 	}
@@ -23,6 +23,30 @@ func TestValidateContextName(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			errString := ""
 			if err := ValidateContextName(tc.name); err != nil {
+				errString = err.Error()
+			}
+			if tc.expectedErr != errString {
+				t.Fatalf("expected %s but got %s", tc.expectedErr, errString)
+			}
+		})
+	}
+}
+
+func TestValidateAPIURL(t *testing.T) {
+	var testCases = []struct {
+		apiURL      string
+		expectedErr string
+	}{
+		{apiURL: "http://test.com", expectedErr: ""},
+		{apiURL: "http://test", expectedErr: ""},
+		{apiURL: "test.com", expectedErr: "invalid url does not contain ://"},
+		{apiURL: "http:/test.com", expectedErr: "invalid url does not contain ://"},
+		{apiURL: "://testcom", expectedErr: "invalid Fn API URL: parse ://testcom: missing protocol scheme"},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.apiURL, func(t *testing.T) {
+			errString := ""
+			if err := ValidateAPIURL(tc.apiURL); err != nil {
 				errString = err.Error()
 			}
 			if tc.expectedErr != errString {
