a6d15df00cfd87469f74a2f8a5ff403679790c04
JadeRedworth <jaderedworth@hotmail.co.uk>
2018-07-17 14:03:32 +0100

    Improve migrate command description and change usage to use latest rather than new

--- commands/migrate.go
+++ commands/migrate.go
@@ -24,10 +24,10 @@ func MigrateCommand() cli.Command {
 
 	return cli.Command{
 		Name:        "migrate",
-		Usage:       "Migrate a local func.yaml file to the new version",
+		Usage:       "Migrate a local func.yaml file to the latest version",
 		Category:    "DEVELOPMENT COMMANDS",
 		Aliases:     []string{"m"},
-		Description: "Migrate will detect the version of the current func.yaml file, to the new version.",
+		Description: "This command will detect the version of a func.yaml file and update it to match the latest version supported by the Fn CLI. Any old or unsupported attributes will be removed, and any new ones may be added. The current func.yaml will be renamed to func.yaml.bak and a new func.yaml created",
 		Action:      m.migrate,
 	}
 }
