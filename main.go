cf1d531cd8afeb3f6a93193ca2ac8230ed7ef98a
Vittoria Placidi <placidi.vittoria@gmail.com>
2018-07-18 15:53:04 +0100

    Added description section to development commands

--- main.go
+++ main.go
@@ -124,7 +124,6 @@ func newFn() *cli.App {
 		
 {{bold "FURTHER HELP:"}}	{{italic "See "}}){{"'"}}{{brightcyan .HelpName}}{{brightcyan " <subcommand> --help"}}{{"'"}}{{italic "for more information about a command."}}{{end}}{{end}}
 `
-
 	app.CommandNotFound = func(c *cli.Context, cmd string) {
 		fmt.Fprintf(os.Stderr, "\n'"+color.Red("%v")+"' is not a Fn Command: "+color.Italic("note the fn CLI command structure has changed, please change your command to use the new structure.\n\n"), cmd)
 		fmt.Fprintf(os.Stderr, color.Bold("FURTHER HELP: ")+color.Italic("See ")+"'"+color.BrightCyan("fn <command> --help")+"'"+color.Italic(" for more information.\n"))
