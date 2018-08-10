f10e8ff94f286225c575eab012183353e2171d38
Tom Coupland <tom.coupland@oracle.com>
2018-08-10 11:56:54 +0100

    Adding invoke command which uses fn invoke to execute fns
    This does not add support for 'fn test', with this change that will
    continue to only work with routes. If we merge this we'll have to loop
    back and change that over to work with fns.
    
    The url to invoke the fn at comes back as an annotation on the fn metadata.

---
+++ commands/invoke.go
@@ -0,0 +1,90 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/fnproject/cli/client"
+	"github.com/fnproject/cli/common"
+	"github.com/fnproject/cli/objects/app"
+	"github.com/fnproject/cli/objects/fn"
+	"github.com/fnproject/cli/run"
+	"github.com/fnproject/fn_go/clientv2"
+	"github.com/fnproject/fn_go/provider"
+	"github.com/urfave/cli"
+)
+
+// FnInvokeEndpointAnnotation is the annotation that exposes the fn invoke endpoint as defined in models/fn.go
+const FnInvokeEndpointAnnotation = "fnproject.io/fn/invokeEndpoint"
+
+type invokeCmd struct {
+	provider provider.Provider
+	client   *clientv2.Fn
+}
+
+// InvokeFnFlags used to invoke and fn
+var InvokeFnFlags = append(run.RunFlags,
+	cli.BoolFlag{
+		Name:  "display-call-id",
+		Usage: "whether display call ID or not",
+	},
+)
+
+// InvokeCommand returns call cli.command
+func InvokeCommand() cli.Command {
+	cl := invokeCmd{}
+	return cli.Command{
+		Name:    "invoke",
+		Usage:   "\tInvoke a remote function",
+		Aliases: []string{"iv"},
+		Before: func(c *cli.Context) error {
+			var err error
+			cl.provider, err = client.CurrentProvider()
+			if err != nil {
+				return err
+			}
+			cl.client = cl.provider.APIClientv2()
+			return nil
+		},
+		ArgsUsage:   "<app-name> <function-name>",
+		Flags:       InvokeFnFlags,
+		Category:    "DEVELOPMENT COMMANDS",
+		Description: "This command explicitly invokes a function.",
+		Action:      cl.Invoke,
+	}
+}
+
+func (cl *invokeCmd) Invoke(c *cli.Context) error {
+	var contentType string
+
+	appName := c.Args().Get(0)
+	fnName := c.Args().Get(1)
+
+	app, err := app.GetAppByName(appName)
+	if err != nil {
+		return err
+	}
+	fn, err := fn.GetFnByName(cl.client, app.ID, fnName)
+	if err != nil {
+		return err
+	}
+
+	content := run.Stdin()
+	wd := common.GetWd()
+
+	if c.String("content-type") != "" {
+		contentType = c.String("content-type")
+	} else {
+		_, ff, err := common.FindAndParseFuncFileV20180707(wd)
+		if err == nil && ff.Content_type != "" {
+			contentType = ff.Content_type
+		}
+	}
+
+	invokeUrl := fn.Annotations[FnInvokeEndpointAnnotation]
+	if invokeUrl == nil {
+		return fmt.Errorf("Fn invoke url annotation not present, %s", FnInvokeEndpointAnnotation)
+	}
+
+	return client.Invoke(cl.provider, invokeUrl.(string), content, os.Stdout, c.String("method"), c.StringSlice("e"), contentType, c.Bool("display-call-id"))
+}
---
+++ test/cli_invoke_test.go
@@ -0,0 +1,31 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/fnproject/cli/testharness"
+)
+
+func TestFnInvokeInvalidImage(t *testing.T) {
+	t.Parallel()
+
+	h := testharness.Create(t)
+	defer h.Cleanup()
+	appName1 := h.NewAppName()
+	funcName1 := h.NewFuncName()
+	h.Fn("create", "app", appName1).AssertSuccess()
+	h.Fn("create", "function", appName1, funcName1, "foo/duffimage:0.0.1").AssertSuccess()
+	h.Fn("invoke", appName1, funcName1).AssertFailed()
+}
+
+func TestFnInvokeValidImage(t *testing.T) {
+	t.Parallel()
+
+	h := testharness.Create(t)
+	defer h.Cleanup()
+	appName1 := h.NewAppName()
+	funcName1 := h.NewFuncName()
+	h.Fn("create", "app", appName1).AssertSuccess()
+	h.Fn("create", "function", appName1, funcName1, "fnproject/hello:latest").AssertSuccess()
+	h.Fn("invoke", appName1, funcName1).AssertSuccess()
+}
