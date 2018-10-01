3c42def4f906f31b68b268a914b9856cff7ba9db
Richard Connon <richard.connon@oracle.com>
2018-10-01 13:25:56 +0100

    Re-add missing function in windows invoke

--- commands/invoke_windows.go
+++ commands/invoke_windows.go
@@ -13,3 +13,11 @@ func stdin() io.Reader {
 	}
 	return os.Stdin
 }
+
+func isTerminal(fd int) bool {
+	kernel32 := syscall.NewLazyDLL("kernel32.dll")
+	procGetConsoleMode := kernel32.NewProc("GetConsoleMode")
+	var st uint32
+	r, _, e := syscall.Syscall(procGetConsoleMode.Addr(), 2, uintptr(fd), uintptr(unsafe.Pointer(&st)), 0)
+	return r != 0 && e == 0
+}
