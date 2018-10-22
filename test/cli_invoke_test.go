2c26e10d2efcc604e934e512ead967233ead62b7
Owen Cliffe <owen.cliffe@oracle.com>
2018-10-22 16:40:51 +0100

    add direct invoke URL to fn invoke

--- test/cli_invoke_test.go
+++ test/cli_invoke_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"github.com/fnproject/cli/testharness"
 	"testing"
+	"strings"
 )
 
 func TestFnInvokeInvalidImage(t *testing.T) {
@@ -27,4 +28,21 @@ func TestFnInvokeValidImage(t *testing.T) {
 	h.Fn("create", "app", appName1).AssertSuccess()
 	h.Fn("create", "function", appName1, funcName1, "fnproject/hello:latest").AssertSuccess()
 	h.Fn("invoke", appName1, funcName1).AssertSuccess()
+}
+
+func TestFnInvokeViaDirectUrl(t *testing.T) {
+	t.Parallel()
+
+	h := testharness.Create(t)
+	defer h.Cleanup()
+	appName1 := h.NewAppName()
+	funcName1 := h.NewFuncName(appName1)
+	h.Fn("create", "app", appName1).AssertSuccess()
+	h.Fn("create", "function", appName1, funcName1, "fnproject/hello:latest").AssertSuccess()
+
+	res := h.Fn("inspect", "function", appName1, funcName1, "--endpoint").AssertSuccess()
+
+	url := strings.TrimSpace(res.Stdout)
+
+	h.Fn("invoke", "--endpoint", url).AssertSuccess()
 }
