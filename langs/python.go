2316ab2b28738e0e82d1b740154f7b0c3710cb83
Reed Allman <reed@rdallman.com>
2018-12-14 15:30:22 -0800

    add json import

--- langs/python.go
+++ langs/python.go
@@ -81,6 +81,7 @@ func (h *PythonLangHelper) IsMultiStage() bool {
 
 const (
 	helloPythonSrcBoilerplate = `import io
+import json
 from fdk import response
 
 
@@ -95,7 +96,7 @@ def handler(ctx, data: io.BytesIO=None):
 
     return response.Response(
         ctx, response_data=json.dumps(
-            {"message": "Hello {0}".format(name)}), 
+            {"message": "Hello {0}".format(name)}),
         headers={"Content-Type": "application/json"}
     )
 `
