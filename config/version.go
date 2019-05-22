1cad9fa4bb53df630f809c1e9399f40b8ee028a3
CI <ci@fnproject.com>
2019-05-22 12:35:34 +0000

    fn CLI: 0.5.79 release [skip ci]

--- config/version.go
+++ config/version.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Version of Fn CLI
-var Version = "0.5.78"
+var Version = "0.5.79"
 
 func GetVersion(versionType string) string {
 	base := "https://github.com/fnproject/cli/releases"
