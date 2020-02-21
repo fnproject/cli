8c3f9a3b6a8d69ad1e9b4b5f00eb0ab7cf8d91b9
Srinidhi Puranik <srinidhi.c.puranik@oracle.com>
2020-02-21 14:37:12 -0800

    Add sample log message to the boilerplate code

--- langs/python.go
+++ langs/python.go
@@ -95,6 +95,7 @@ func (h *PythonLangHelper) IsMultiStage() bool {
 const (
 	helloPythonSrcBoilerplate = `import io
 import json
+import logging
 
 from fdk import response
 
@@ -105,8 +106,9 @@ def handler(ctx, data: io.BytesIO=None):
         body = json.loads(data.getvalue())
         name = body.get("name")
     except (Exception, ValueError) as ex:
-        print(str(ex))
+        logging.getLogger().info('error parsing json payload: ' + str(ex))
 
+    logging.getLogger().info("Inside Python Hello World function")
     return response.Response(
         ctx, response_data=json.dumps(
             {"message": "Hello {0}".format(name)}),
