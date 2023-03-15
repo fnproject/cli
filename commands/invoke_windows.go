0333655bc4d78cfbeea8b52f9dc5d11e619bf299
Sunny Sethi <sunny.sethi@oracle.com>
2023-03-15 15:55:04 +0530

    reverting back commented code for windows build

--- commands/invoke_windows.go
+++ commands/invoke_windows.go
@@ -13,30 +13,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// +build windows
 
 package commands
 
 import (
 	"io"
 	"os"
+	"syscall"
+	"unsafe"
 )
 
 func stdin() io.Reader {
-	/*
 	if isTerminal(int(os.Stdin.Fd())) {
 		return nil
 	}
-	 */
 	return os.Stdin
 }
 
 func isTerminal(fd int) bool {
-	return true
-	/*
 	kernel32 := syscall.NewLazyDLL("kernel32.dll")
 	procGetConsoleMode := kernel32.NewProc("GetConsoleMode")
 	var st uint32
 	r, _, e := syscall.Syscall(procGetConsoleMode.Addr(), 2, uintptr(fd), uintptr(unsafe.Pointer(&st)), 0)
 	return r != 0 && e == 0
-	 */
 }
