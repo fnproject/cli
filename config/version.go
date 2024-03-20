4d2ce274744d36e1d86d4f21151731999ddbe1ac
CI <ci@fnproject.com>
2024-03-20 10:46:12 +0000

    fn CLI: 0.6.30 release [skip ci]

--- config/version.go
+++ config/version.go
@@ -25,7 +25,7 @@ import (
 )
 
 // Version of Fn CLI
-var Version = "0.6.29"
+var Version = "0.6.30"
 
 func GetVersion(versionType string) string {
 	base := "https://github.com/fnproject/cli/releases"
